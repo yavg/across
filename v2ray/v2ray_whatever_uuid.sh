ccefaa8937b3cabcf6e6c227d724274f4e376e9b
Mcl <mixool0204@gmail.com>
2020-11-03 22:21:23 +0800

    acme standalone

--- v2ray/v2ray_whatever_uuid.sh
+++ v2ray/v2ray_whatever_uuid.sh
@@ -6,11 +6,9 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin; export 
 0. 通过安装v2ray+caddy同时配置vless + trojan + ss+v2ray-plugin + naiveproxy服务端，一共配置了了7种模式，其中vless配置了3种传输方式，trojan配置了2种传输方式，均使用443端口
 1. 更多配置参考：https://github.com/lxhao61/integrated-examples https://github.com/v2fly/v2ray-examples
 2. 参数说明：
-    cloudflare_Email_ Address: 可在cloudflare账户信息查看
-    cloudflare_Global_API_Key: 可在cloudflare账户信息页面API-Tokens下查看
     uuid: 作为服务端账号和密码参数，uuid-[vless|vlessh2|trojan|ss]作为服务端路径参数，其它客户端参数查看输出信息。自用uuid务必妥善保存，如有分享需求，建议生成一个分享专用的uuid: cat /proc/sys/kernel/random/uuid
-    my.domain.com: 托管在cloudflare的域名，证书使用acme.sh的DNS API模式申请，本脚本只支持使用cloudflare的API
-3. install: bash <(curl -s https://raw.githubusercontent.com/mixool/across/master/v2ray/v2ray_whatever_uuid.sh) cloudflare_Email_Address cloudflare_Global_API_Key uuid my.domain.com
+    my.domain.com: 域名，证书使用acme.sh的standalone webserver模式申请, 如有其它服务运在80端口，按需修改185行的pre-hook和post-hook参数
+3. install: bash <(curl -s https://raw.githubusercontent.com/mixool/across/master/v2ray/v2ray_whatever_uuid.sh) uuid my.domain.com
 4. uninstall: 
     apt purge caddy -y
     bash <(curl https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh) --remove; systemctl disable v2ray; rm -rf /usr/local/etc/v2ray /var/log/v2ray
@@ -21,8 +19,8 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin; export 
 trap 'rm -f "$TMPFILE"' EXIT; TMPFILE=$(mktemp) || exit 1
 
 ########
-[[ $# != 4 ]] && echo Err  !!! Useage: bash this_script.sh cloudflare_Email_Address cloudflare_Global_API_Key uuid my.domain.com && exit 1
-export CF_Email="$1" && export CF_Key="$2" && uuid="$3" && domain="$4"
+[[ $# != 2 ]] && echo Err  !!! Useage: bash this_script.sh uuid my.domain.com && exit 1
+uuid="$1" && domain="$2"
 xtlsflow="xtls-rprx-direct" && ssmethod="none"
 vlesspath="${uuid}-vless"
 vlessh2path="${uuid}-vlessh2"
@@ -184,7 +182,7 @@ EOF
 apt install socat -y
 curl https://get.acme.sh | sh && source  ~/.bashrc
 /root/.acme.sh/acme.sh --upgrade --auto-upgrade
-/root/.acme.sh/acme.sh --issue --dns dns_cf --keylength ec-256 -d $domain --post-hook "/root/.acme.sh/acme.sh --installcert -d $domain --ecc --fullchain-file /usr/local/etc/v2ray/v2ray.crt --key-file /usr/local/etc/v2ray/v2ray.key --reloadcmd \"service v2ray restart\""
+/root/.acme.sh/acme.sh --issue -d $domain --standalone --keylength ec-256 --pre-hook "systemctl stop caddy v2ray" --post-hook "/root/.acme.sh/acme.sh --installcert -d $domain --ecc --fullchain-file /usr/local/etc/v2ray/v2ray.crt --key-file /usr/local/etc/v2ray/v2ray.key --reloadcmd \"systemctl restart caddy v2ray\""
 chown -R nobody:nogroup /usr/local/etc/v2ray || chown -R nobody:nobody /usr/local/etc/v2ray
 
 # systemctl service info
