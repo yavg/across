9f5a9513f563f8065a025719531c26255ddb39be
Mcl <mixool0204@gmail.com>
2020-11-05 12:14:17 +0800

    add vmess

--- v2ray/v2ray_whatever_uuid.sh
+++ v2ray/v2ray_whatever_uuid.sh
@@ -3,10 +3,10 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin; export 
 
 # Tips
 [ 0 -eq 1 ] && {
-0. 通过安装v2ray+caddy同时配置vless + trojan + ss+v2ray-plugin + naiveproxy服务端，一共配置了了7种模式，其中vless配置了3种传输方式，trojan配置了2种传输方式，均使用443端口
+0. 通过安装v2ray+caddy同时配置vless + trojan + ss+v2ray-plugin + naiveproxy服务端，一共配置了了10种模式，其中vless和vmess各配置了3种传输方式，trojan配置了2种传输方式，均使用443端口
 1. 更多配置参考：https://github.com/lxhao61/integrated-examples https://github.com/v2fly/v2ray-examples
 2. 参数说明：
-    uuid: 作为服务端账号和密码参数，uuid-[vless|vlessh2|trojan|ss]作为服务端路径参数，其它客户端参数查看输出信息。自用uuid务必妥善保存，如有分享需求，建议生成一个分享专用的uuid: cat /proc/sys/kernel/random/uuid
+    uuid: 作为服务端账号和密码参数，uuid-[vless|vlessh2|vmesstcp|vmess|vmessh2|trojan|ss]作为服务端路径参数，其它客户端参数查看输出信息。自用uuid务必妥善保存，如有分享需求，建议生成一个分享专用的uuid: cat /proc/sys/kernel/random/uuid
     my.domain.com: 域名，证书使用acme.sh的standalone webserver模式申请, 如有其它服务运在80端口，按需修改185行的pre-hook和post-hook参数
 3. install: bash <(curl -s https://raw.githubusercontent.com/mixool/across/master/v2ray/v2ray_whatever_uuid.sh) uuid my.domain.com
 4. uninstall: 
@@ -22,9 +22,12 @@ trap 'rm -f "$TMPFILE"' EXIT; TMPFILE=$(mktemp) || exit 1
 [[ $# != 2 ]] && echo Err  !!! Useage: bash this_script.sh uuid my.domain.com && exit 1
 uuid="$1" && domain="$2"
 xtlsflow="xtls-rprx-direct" && ssmethod="none"
+trojanpath="${uuid}-trojan"
 vlesspath="${uuid}-vless"
 vlessh2path="${uuid}-vlessh2"
-trojanpath="${uuid}-trojan"
+vmesstcppath="${uuid}-vmesstcp"
+vmesswspath="${uuid}-vmess"
+vmessh2path="${uuid}-vmessh2"
 shadowsockspath="${uuid}-ss"
 ########
 
@@ -44,7 +47,9 @@ cat <<EOF >/usr/local/etc/v2ray/config.json
                     {"dest": "/usr/local/etc/v2ray/trojan"},
                     {"dest": "/usr/local/etc/v2ray/trojanws","path": "/$trojanpath"},
                     {"dest": "/usr/local/etc/v2ray/vlessws","path": "/$vlesspath"},
-                    {"dest": 50002,"path": "/$shadowsockspath"}
+					{"dest": "/usr/local/etc/v2ray/vmessws","path": "/$vmesswspath"},
+					{"dest": "/usr/local/etc/v2ray/vmesstcp","path": "/$vmesstcppath"},
+                    {"dest": 50003,"path": "/$shadowsockspath"}
                 ]
             },
             "streamSettings": {"network": "tcp","security": "xtls","xtlsSettings": {"alpn": ["h2","http/1.1"],"certificates": [{"certificateFile": "/usr/local/etc/v2ray/v2ray.crt","keyFile": "/usr/local/etc/v2ray/v2ray.key"}]}}
@@ -69,13 +74,28 @@ cat <<EOF >/usr/local/etc/v2ray/config.json
             "settings": {"clients": [{"id": "$uuid"}],"decryption": "none"},
             "streamSettings": {"network": "h2","httpSettings": {"host": ["$domain"],"path": "/$vlessh2path"}}
         },
+		{
+            "listen": "/usr/local/etc/v2ray/vmesstcp","protocol": "vmess",
+            "settings": {"clients": [{"id": "$uuid"}]},
+			"streamSettings": {"network": "tcp","security": "none","tcpSettings": {"acceptProxyProtocol": true,"header": {"type": "http","request": {"path": ["/$vmesstcppath"]}}}}
+        },
+		{
+            "listen": "/usr/local/etc/v2ray/vmessws","protocol": "vmess",
+            "settings": {"clients": [{"id": "$uuid"}]},
+            "streamSettings": {"network": "ws","security": "none","wsSettings": {"path": "/$vmesswspath"}}
+        },
         {
-            "port": "50002","listen": "127.0.0.1","tag": "onetag","protocol": "dokodemo-door",
+            "port": 50002,"listen": "127.0.0.1","protocol": "vmess",
+            "settings": {"clients": [{"id": "$uuid"}]},
+            "streamSettings": {"network": "h2","httpSettings": {"host": ["$domain"],"path": "/$vmessh2path"}}
+        },
+        {
+            "port": "50003","listen": "127.0.0.1","tag": "onetag","protocol": "dokodemo-door",
             "settings": {"address": "v1.mux.cool","network": "tcp","followRedirect": false},
             "streamSettings": {"security": "none","network": "ws","wsSettings": {"path": "/$shadowsockspath"}}
         },
         {
-            "port": 50003,"listen": "127.0.0.1","protocol": "shadowsocks",
+            "port": 50004,"listen": "127.0.0.1","protocol": "shadowsocks",
             "settings": {"method": "$ssmethod","password": "$uuid","network": "tcp,udp"},
             "streamSettings": {"security": "none","network": "domainsocket","dsSettings": {"path": "/usr/local/etc/v2ray/ss"}}
         },
@@ -151,6 +171,23 @@ cat <<EOF >/etc/caddy/Caddyfile.json
                                           },
                                           "upstreams": [{
                                             "dial": "127.0.0.1:50001"
+                                          }]
+                                        }],
+                                        "terminal": true
+                                    },
+									{
+                                        "match": [{"path": ["/$vmessh2path"]}],
+                                        "handle": [{
+                                          "handler": "reverse_proxy",
+                                          "transport": {
+                                            "protocol": "http",
+                                            "keep_alive": {
+                                              "enabled": false
+                                            },
+                                            "versions": ["h2c"]
+                                          },
+                                          "upstreams": [{
+                                            "dial": "127.0.0.1:50002"
                                           }]
                                         }],
                                         "terminal": true
