126c241bf0f2c2fbbcb71cec91efcc4999d8a47e
Mcl <mixool0204@gmail.com>
2020-11-14 20:43:28 +0800

    init.

--- kvmbbr/bbr.sh
+++ kvmbbr/bbr.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Usage: debian 10 & 9 && linux-image-cloud-amd64 bbr && personal use only
-#   bash <(curl -s https://raw.githubusercontent.com/mixool/across/master/kvmbbr/bbr.sh)        # 仅开启bbr
-#   bash <(curl -s https://raw.githubusercontent.com/mixool/across/master/kvmbbr/bbr.sh) cloud  # 危险操作: 安装cloud内核并开启bbr
-#   bash <(curl -s https://raw.githubusercontent.com/mixool/across/master/kvmbbr/bbr.sh) old    # 危险操作: 卸载未使用内核并开启bbr
+#   bash <(curl -s https://raw.githubusercontent.com/mixool/across/master/kvmbbr/bbr.sh)              # 仅开启bbr
+#   bash <(curl -s https://raw.githubusercontent.com/mixool/across/master/kvmbbr/bbr.sh) cloud        # 危险操作: 安装cloud内核并开启bbr
+#   bash <(curl -s https://raw.githubusercontent.com/mixool/across/master/kvmbbr/bbr.sh) removeold    # 危险操作: 卸载未使用内核并开启bbr
 ###
 
 # only root can run this script
@@ -24,13 +24,14 @@ if [[ "$1" == "cloud" ]]; then
 fi
 
 # old kernel remove 
-if [[ "$1" == "old" ]]; then
+if [[ "$1" == "removeold" ]]; then
     echo $(dpkg --list | grep linux-image | awk '{ print $2 }' | sort -V | sed -n '/'`uname -r`'/q;p') $(dpkg --list | grep linux-headers | awk '{ print $2 }' | sort -V | sed -n '/'"$(uname -r | sed "s/\([0-9.-]*\)-\([^0-9]\+\)/\1/")"'/q;p') | xargs apt --purge -y autoremove
 fi
 
 # bbr 
-cat /etc/sysctl.conf | grep -q "net.core.default_qdisc = fq" || echo "net.core.default_qdisc = fq" >> /etc/sysctl.conf
+cat /etc/sysctl.conf | grep -q "net.core.default_qdisc = .*" && sed -i "s/net.core.default_qdisc = .*/net.core.default_qdisc = fq/g" /etc/sysctl.conf || echo "net.core.default_qdisc = fq" >> /etc/sysctl.conf
 cat /etc/sysctl.conf | grep -q "net.ipv4.tcp_congestion_control = bbr" || echo "net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf
+echo /etc/sysctl.conf info:
 sysctl -p
 
 # end
