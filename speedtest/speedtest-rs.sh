1dd14acd33029b8cb12f93fda6bc1acd20074759
Mcl <mixool0204@gmail.com>
2020-12-09 00:51:18 +0800

    Create speedtest-rs.sh

---
+++ speedtest/speedtest-rs.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# Wiki: https://github.com/zhanghanyun/speedtest-rs
+# Usage: bash <(curl -s https://raw.githubusercontent.com/mixool/across/master/speedtest/speedtest-rs.sh) my.domain.com
+# Uninstall: apt purge caddy -y; systemctl stop speedtest-rs; systemctl disable speedtest-rs; rm -rf /etc/systemd/system/speedtest-rs.service /usr/bin/speedtest-rs
+
+# tempfile & rm it when exit
+trap 'rm -f "$TMPFILE"' EXIT && TMPFILE=$(mktemp) || exit 1
+
+# domain
+[[ $# == 1 ]] && domain="$1" || { echo Err !!! Useage: bash this_script.sh my.domain.com; exit 1; }
+
+# speedtest-rs 
+URL="$(wget -qO- https://api.github.com/repos/zhanghanyun/speedtest-rs/releases/latest | grep -E "browser_download_url.*linux-amd64" | cut -f4 -d\")"
+rm -rf /usr/bin/speedtest-rs
+wget -O /usr/bin/speedtest-rs $URL && chmod +x /usr/bin/speedtest-rs
+
+cat <<EOF > /etc/systemd/system/speedtest-rs.service
+[Unit]
+Description=speedtest-rs
+[Service]
+ExecStart=/usr/bin/speedtest-rs --port 18088
+Restart=always
+User=root
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# caddy
+caddyURL="$(wget -qO-  https://api.github.com/repos/caddyserver/caddy/releases | grep -E "browser_download_url.*linux_amd64\.deb" | cut -f4 -d\" | head -n1)"
+wget -O $TMPFILE $caddyURL && dpkg -i $TMPFILE
+
+cat <<EOF >/etc/caddy/Caddyfile
+$domain
+root /usr/share/caddy
+reverse_proxy 127.0.0.1:18088
+}
+EOF
+
+# systemctl service info
+systemctl enable caddy v2ray && systemctl restart caddy v2ray && sleep 3 && systemctl status caddy v2ray | grep -A 2 "service"
