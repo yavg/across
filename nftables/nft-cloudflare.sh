77b75288510c13859cfdf2b730151a82123a562c
-- <mixool0204@gmail.com>
2021-01-25 18:15:14 +0800

    fix err && update
    https://unix.stackexchange.com/questions/581964/create-dynamic-blacklist-with-nftables

--- nftables/nft-cloudflare.sh
+++ nftables/nft-cloudflare.sh
@@ -32,45 +32,43 @@ define SAFE_TRAFFIC_IPS = {
 table inet my_table {
     set blackhole {
         type ipv4_addr
-        size 65536
         flags dynamic,timeout
-        timeout 5m
+        timeout 5d
     }
     
     chain my_input {
         type filter hook input priority 0; policy drop;
         
-        ip6 nexthdr icmpv6 icmpv6 type echo-request limit rate 1/second accept
-        ip6 nexthdr icmpv6 icmpv6 type echo-request counter drop
-        
-        ip protocol icmp icmp type echo-request limit rate 1/second accept
-        ip protocol icmp icmp type echo-request counter drop
-        
+        iif lo accept
+        ip saddr @blackhole counter set update ip saddr @blackhole counter drop
         ct state {established, related} counter accept
         ct state invalid drop
         
+        ip6 nexthdr icmpv6 icmpv6 type echo-request limit rate 1/second accept
         ip6 nexthdr icmpv6 icmpv6 type { destination-unreachable, packet-too-big, time-exceeded, parameter-problem, echo-reply, nd-router-advert, nd-neighbor-solicit, nd-neighbor-advert } accept
+        
+        ip protocol icmp icmp type echo-request limit rate 1/second accept
         ip protocol icmp icmp type { destination-unreachable, router-advertisement, time-exceeded, parameter-problem } accept
         
-        iif lo accept
-
-        tcp dport { http, https } ip saddr \$SAFE_TRAFFIC_IPS accept
-        udp dport { http, https } ip saddr \$SAFE_TRAFFIC_IPS accept
+        tcp dport { http, https } ip saddr \$SAFE_TRAFFIC_IPS counter accept
+        udp dport { http, https } ip saddr \$SAFE_TRAFFIC_IPS counter accep
         
-        tcp dport $(cat /etc/ssh/sshd_config | grep -oE "^Port [0-9]*$" | grep -oE "[0-9]*" || echo 22) limit rate over 5/minute add @blackhole { ip saddr }
-        ip saddr @blackhole counter drop
-        tcp dport $(cat /etc/ssh/sshd_config | grep -oE "^Port [0-9]*$" | grep -oE "[0-9]*" || echo 22) ct state new limit rate 5/minute counter accept
+        tcp flags syn tcp dport $(cat /etc/ssh/sshd_config | grep -oE "^Port [0-9]*$" | grep -oE "[0-9]*" || echo 22) meter aaameter { ip saddr ct count over 5 } add @blackhole { ip saddr } counter drop
+        tcp flags syn tcp dport $(cat /etc/ssh/sshd_config | grep -oE "^Port [0-9]*$" | grep -oE "[0-9]*" || echo 22) meter bbbmeter { ip saddr limit rate over 5/minute } add @blackhole { ip saddr } counter drop
+        tcp dport $(cat /etc/ssh/sshd_config | grep -oE "^Port [0-9]*$" | grep -oE "[0-9]*" || echo 22) counter accept
         
         counter comment "count dropped packets"
     }
     
     chain my_forward {
         type filter hook forward priority 0; policy accept;
+        ip daddr @blackhole counter reject
         counter comment "count accepted packets"
     }
     
     chain my_output {
         type filter hook output priority 0; policy accept;
+        ip daddr @blackhole counter reject
         counter comment "count accepted packets"
     }
 }
