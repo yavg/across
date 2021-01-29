e352ad71c7e490a7ae7c8356671da41e42a6f875
-- <mixool0204@gmail.com>
2021-01-29 14:03:11 +0800

    Update blackholeips.sh

--- personal/blackholeips.sh
+++ personal/blackholeips.sh
@@ -2,7 +2,7 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin; export PATH
 # Usage: 把爆破我SSH的IP提交到GIST: https://gist.githubusercontent.com/mixool/495619a89bdd4da11e80115a82d177fe/raw
 # wget -O /opt/blackholeips.sh https://raw.githubusercontent.com/mixool/across/master/personal/blackholeips.sh && chmod +x /opt/blackholeips.sh && /opt/blackholeips.sh $gist_token
-## gist_token=***********; (crontab -l; echo "1 * * * * /opt/blackholeips.sh $gist_token") | crontab -
+## gist_token=***********; (crontab -l; echo "0 8 * * * /opt/blackholeips.sh $gist_token") | crontab -
 
 # tempfile & rm it when exit
 trap 'rm -f "$TMPFILE"' EXIT; TMPFILE=$(mktemp) || exit 1
